ef3871346e32f66cff1781945b78457148305fbf
Gonzalo Aguado Torres <gonagutor@gmail.com>
2023-09-03 21:18:23 +0200

    Change env.go to run on docker enviroment variables

--- utils/env.go
+++ utils/env.go
@@ -11,25 +11,26 @@ func SetupEnv() {
 	godotenv.Load(".env")
 	env, err := godotenv.Read(".env")
 	if err != nil {
-		log.Fatalf(color.Ize(color.Red, "No .env file. Copy .env.example to .env or create a new one"))
-	} else {
-		if env["POSTGRES_PASSWORD"] == "" {
-			log.Fatalf(color.Ize(color.Red, "POSTGRE_PASSWORD missing on .env file"))
-		}
-		if env["POSTGRES_USER"] == "" {
-			log.Fatalf(color.Ize(color.Red, "POSTGRES_USER missing on .env file"))
-		}
-		if env["POSTGRES_DB"] == "" {
-			log.Fatalf(color.Ize(color.Red, "POSTGRES_DB missing on .env file"))
-		}
-		if env["POSTGRES_PORT"] == "" {
-			log.Fatalf(color.Ize(color.Red, "POSTGRES_PORT missing on .env file"))
-		}
-		if env["POSTGRES_HOST"] == "" {
-			log.Fatalf(color.Ize(color.Red, "POSTGRES_HOST missing on .env file"))
-		}
-		if env["JWT_SECRET"] == "" {
-			log.Fatalf(color.Ize(color.Red, "JWT_SECRET missing on .env file"))
-		}
+		log.Println(color.Ize(color.Yellow, "No .env file. Copy .env.example to .env or create a new one. You can ignore this message if running on a docker container"))
 	}
+
+	if env["POSTGRES_PASSWORD"] == "" {
+		log.Fatalf(color.Ize(color.Red, "POSTGRES_PASSWORD missing from enviroment"))
+	}
+	if env["POSTGRES_USER"] == "" {
+		log.Fatalf(color.Ize(color.Red, "POSTGRES_USER missing from enviroment"))
+	}
+	if env["POSTGRES_DB"] == "" {
+		log.Fatalf(color.Ize(color.Red, "POSTGRES_DB missing from enviroment"))
+	}
+	if env["POSTGRES_PORT"] == "" {
+		log.Fatalf(color.Ize(color.Red, "POSTGRES_PORT missing from enviroment"))
+	}
+	if env["POSTGRES_HOST"] == "" {
+		log.Fatalf(color.Ize(color.Red, "POSTGRES_HOST missing from enviroment"))
+	}
+	if env["JWT_SECRET"] == "" {
+		log.Fatalf(color.Ize(color.Red, "JWT_SECRET missing from enviroment"))
+	}
+
 }
