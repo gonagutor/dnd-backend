edfceb6f067e1278cbcb71c67a020d24ebb5ba1a
DamSergio <dam_sergioballesteros@riberadeltajo.es>
2024-02-07 21:06:30 +0100

    implements GetPages

--- handlers/v1/user/get_pages.go
+++ handlers/v1/user/get_pages.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func GetAll(ctx *fiber.Ctx) error {
+func GetPages(ctx *fiber.Ctx) error {
 	user := ctx.Locals("user").(*models.User)
 	if user.Role != "admin" {
 		return ctx.Status(fiber.StatusForbidden).JSON(fiber.Map{
@@ -17,23 +17,14 @@ func GetAll(ctx *fiber.Ctx) error {
 		})
 	}
 
-	users, err := models.GetAllUsers(ctx)
-	if err != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error":   "COULD_NOT_GET_USERS",
-			"message": "Users could not be retrieved",
-		})
-	}
-
 	page, pageSize := utils.Pagination(ctx)
 	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
-		"code":    "USERS_FOUND",
-		"message": "Users found",
+		"code":    "PAGES_FOUND",
+		"message": "Pages found",
 		"pagination": fiber.Map{
 			"page":     page,
 			"maxPages": math.Ceil(float64(models.CountUsers()) / float64(pageSize)),
 			"pageSize": pageSize,
 		},
-		"data": users,
 	})
 }
