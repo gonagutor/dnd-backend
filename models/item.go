b75ad72e0a97a4921c1b918e79e9edaee8553fe2
DamSergio <dam_sergioballesteros@riberadeltajo.es>
2024-08-08 12:51:21 +0200

    item not null

--- models/item.go
+++ models/item.go
@@ -9,40 +9,40 @@ import (
 type TranslatableField map[string]string
 
 type Cost struct {
-	Copper   uint8 `json:"copper" bson:"copper" default:"0"`
-	Silver   uint8 `json:"silver" bson:"silver" default:"0"`
-	Gold     uint8 `json:"gold" bson:"gold" default:"0"`
-	Electrum uint8 `json:"electrum" bson:"electrum" default:"0"`
-	Platinum uint8 `json:"platinum" bson:"platinum" default:"0"`
+	Copper   uint32 `gorm:"not null" json:"copper" bson:"copper"`
+	Electrum uint32 `gorm:"not null" json:"electrum" bson:"electrum"`
+	Gold     uint32 `gorm:"not null" json:"gold" bson:"gold"`
+	Silver   uint32 `gorm:"not null" json:"silver" bson:"silver"`
+	Platinum uint32 `gorm:"not null" json:"platinum" bson:"platinum"`
 }
 
 type Damage struct {
-	Count uint8  `json:"count" bson:"count" validate:"required"`
-	Faces uint8  `json:"faces" bson:"faces" validate:"required"`
-	Range uint8  `json:"range" bson:"range" default:"0"`
-	Type  string `json:"type" bson:"type" validate:"required"`
+	Count uint8  `gorm:"not null" json:"count" bson:"count"`
+	Faces uint8  `gorm:"not null" json:"faces" bson:"faces"`
+	Range uint16 `gorm:"not null" json:"range" bson:"range"`
+	Type  string `gorm:"not null" json:"type" bson:"type"`
 }
 
 type Combat struct {
-	Damage Damage `gorm:"type:jsonb" json:"damage" bson:"damage" validate:"optional,dive"`
-	Ac     uint8  `json:"ac" bson:"ac" validate:"optional" default:"0"`
+	Damage Damage `gorm:"type:jsonb;not null" json:"damage" bson:"damage"`
+	Ac     uint8  `gorm:"not null" json:"ac" bson:"ac"`
 }
 
 type Item struct {
 	gorm.Model
 
-	ID          string            `gorm:"primary_key" json:"id" bson:"id" validate:"required,unique"`
-	Name        pgtype.JSONBCodec `gorm:"type:jsonb" json:"name" bson:"name" validate:"required,dive"`
-	Description pgtype.JSONBCodec `gorm:"type:jsonb" json:"description" bson:"description" validate:"required,dive"`
-	Source      string            `json:"source" bson:"source" validate:"required" default:"homebrew"`
-	Page        *uint16           `json:"page" bson:"page" validate:"omitempty,min=1"`
-	Tags        pq.StringArray    `gorm:"type:text" json:"tags" bson:"tags" validate:"required"`
-	Rarity      uint8             `json:"rarity" bson:"rarity" validate:"required" default:"0"`
-	Weight      float32           `json:"weight" bson:"weight" validate:"required" default:"0.0"`
-	Atunement   bool              `json:"atunement" bson:"atunement" validate:"required" default:"false"`
-	Cost        Cost              `gorm:"type:jsonb" json:"cost" bson:"cost" validate:"required,dive"`
-	Contains    pq.StringArray    `gorm:"type:text" json:"contains" bson:"contains" validate:"optional,dive"`
-	Combat      Combat            `gorm:"type:jsonb" json:"combat" bson:"combat" validate:"optional,dive"`
+	ID          string            `gorm:"primary_key;not null" json:"id" bson:"id"`
+	Name        pgtype.JSONBCodec `gorm:"type:jsonb;not null" json:"name" bson:"name"`
+	Description pgtype.JSONBCodec `gorm:"type:jsonb;not null" json:"description" bson:"description"`
+	Source      string            `gorm:"not null" json:"source" bson:"source"`
+	Page        *uint16           `json:"page" bson:"page"`
+	Tags        pq.StringArray    `gorm:"type:text;not null" json:"tags" bson:"tags"`
+	Rarity      uint8             `gorm:"not null" json:"rarity" bson:"rarity"`
+	Weight      float32           `gorm:"not null" json:"weight" bson:"weight"`
+	Atunement   bool              `gorm:"not null" json:"atunement" bson:"atunement"`
+	Cost        Cost              `gorm:"type:jsonb;not null" json:"cost" bson:"cost"`
+	Contains    pq.StringArray    `gorm:"type:text;not null" json:"contains" bson:"contains"`
+	Combat      Combat            `gorm:"type:jsonb;not null" json:"combat" bson:"combat"`
 }
 
 func (i *Item) Validate() error {
