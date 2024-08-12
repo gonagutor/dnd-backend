47571e7b5c50e096737d0b73ef982d7cb86fc19e
DamSergio <dam_sergioballesteros@riberadeltajo.es>
2024-08-12 15:53:36 +0200

    feat: items endpoints

--- routes/item.go
+++ routes/item.go
@@ -11,6 +11,7 @@ func SetupItemRoutes(router fiber.Router) {
 	itemRouter := router.Group("/item")
 
 	itemRouter.Get("/", protected.New(protected.Config{}), item.GetAll)
+	itemRouter.Get("/:user_id", protected.New(protected.Config{}), item.GetFromUser)
 	itemRouter.Post("/", protected.New(protected.Config{}), item.Create)
 	itemRouter.Put("/:item_id", protected.New(protected.Config{}), item.Edit)
 	itemRouter.Delete("/:item_id", protected.New(protected.Config{}), item.Delete)
